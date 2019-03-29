37939c67261bdac1de305c247c199f3628e7ab76
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2019-03-29 15:20:17 -0700

    Ensure context is eia when using EIA

--- keras/backend/mxnet_backend.py
+++ keras/backend/mxnet_backend.py
@@ -5421,6 +5421,7 @@ def get_model():
             if not self._module.binded:
                 # allow prediction without compiling the model using different binding
                 if phase == 'pred' and (self._context[0] == "eia" or not self.compiled):
+                    self._module._context = [mx.eia()]
                     self._module.bind(data_shapes=data_shapes, label_shapes=None,
                                       for_training=False)
                     self._set_weights()
